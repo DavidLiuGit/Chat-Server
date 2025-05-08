e8e102a0224edb7dcb2473d204cee1f84b7ba3ab
DL <dvdgege5@gmail.com>
2025-05-07 22:30:00 -0500

    Impl retriever example
    Start impl of chat & chain
    
    Lint chain.py

--- app/core/chain.py
+++ app/core/chain.py
@@ -1,14 +1,32 @@
 """
-# TODO: Implement logic to return a configured LCEL chain
+Here's the the bulk of the "magic" happens. Implement `build_chain()`.
+
+`@lru_cache` is used to make the result effectively a singleton. Subsequent identical
+calls to `build_chain()` will simply return the instance of the chain that already exists.
 """
 
 from functools import lru_cache
 
 from chat_chain.chain import ChatChain, ChatChainProps
 
-from app.core.llm import *
+from app.core.llm import get_language_model
+from app.core.retriever import build_retriever
 
 
 @lru_cache
-def build_chain() -> ChatChain:
-    raise NotImplementedError("This function is not implemented yet.")
+def build_chain():
+    """
+    Build an instance of `ChatChainProps`
+    """
+    qa_llm = get_language_model()
+    retriever = build_retriever()
+    chat_chain_props = ChatChainProps(
+        chat_llm=qa_llm,
+        chat_prompt=(
+            "You are a helpful assistant. Answer the following question based on the"
+            " provided context. If you don't know the answer, just say that you don't know."
+            " Don't make up an answer."
+        ),
+        retriever=retriever,
+    )
+    return ChatChain(chat_chain_props)
--- app/core/retriever.py
+++ app/core/retriever.py
@@ -1,4 +1,32 @@
 from langchain_core.retrievers import BaseRetriever
 
+from langchain_logseq.loaders.journal_filesystem_loader import LogseqJournalFilesystemLoader
+from langchain_logseq.loaders.journal_loader_input import LogseqJournalLoaderInput
+from langchain_logseq.retrievers.contextualizer import RetrieverContextualizer, RetrieverContextualizerProps
+from langchain_logseq.retrievers.journal_date_range_retriever import LogseqJournalDateRangeRetriever
+
+from app.core.llm import get_fast_language_model
+
+
 def build_retriever() -> BaseRetriever:
-    raise NotImplementedError("This function is not implemented yet.")
+    """
+    Build a `Retriever` to be used to contexualize the chat.
+    """
+    llm = get_fast_language_model()
+    loader = LogseqJournalFilesystemLoader(".")
+    contextualizer = RetrieverContextualizer(
+        RetrieverContextualizerProps(
+            llm=llm,
+            prompt=(
+                "Given the user_input, and optional chat_history, create an query object based"
+                "on the schema provided, if you believe it is relevant. Do not include anything"
+                "except for the schema, serialized as JSON. Do not answer the question directly"
+            ),
+            output_schema=LogseqJournalLoaderInput,
+            enable_chat_history=True,
+        )
+    )
+    return LogseqJournalDateRangeRetriever(
+        contextualizer,
+        loader,
+    )
