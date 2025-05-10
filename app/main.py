bb1b078ed3e80ae259b89e32b8e021ce64859a0b
DL <dvdgege5@gmail.com>
2025-05-09 22:37:22 -0500

    load_dotenv in main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,8 @@
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 from fastapi import FastAPI
 from app.api.v1.routes import router as api_router
 
@@ -9,12 +14,13 @@ app.include_router(api_router, prefix="/v1")
 from contextlib import asynccontextmanager
 from app.core.chain import build_chain
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup logic
     build_chain()  # ensures chain is built before serving requests
-    
+
     # Let FastAPI do its thing
     yield
-    
+
     # Shutdown logic, if needed
